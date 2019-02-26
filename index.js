bbf529c038ed3806ef5a3f09d6f93f52c52ce469
Deepak Sharma <sharmadeepak1999@gmail.com>
2019-02-27 02:55:43 +0530

    second publish

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000 || process.env.PORT, () => {
+app.listen( process.env.PORT || 3000, () => {
 	console.log("Server Started at port 3000");
 });
